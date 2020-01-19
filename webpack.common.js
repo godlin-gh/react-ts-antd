3335e02654d3e566a35da1a633e525fbaf699db7
godlin <ganlin.gl@alibaba-inc.com>
2020-01-19 13:10:44 +0800

    Init

---
+++ webpack.common.js
@@ -0,0 +1,51 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+
+  entry: {
+    index: 'src/index.tsx'
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      title: 'Output Management'
+    })
+  ],
+
+  output: {
+    filename: '[name].[hash].js',
+    path: path.resolve(__dirname, 'dist')
+  },
+
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [
+          'awesome-typescript-loader'
+        ],
+        exclude: /node_modules/
+      },
+      // {
+      //   test: /\.css$/,
+      //   use: [
+      //     'style-loader',
+      //     'css-loader'
+      //   ]
+      // },
+      // {
+      //   test: /\.(png|svg|jpg|gif|jpeg)$/,
+      //   use: [
+      //     'file-loader'
+      //   ]
+      // }
+    ]
+  }
+
+}
